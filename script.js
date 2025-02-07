0c2f644a20487d9f85f97f2446eb6a504a50a71c
loucas <loucaslbp@hotmail.com>
2025-02-08 00:16:02 +0100

    Correction du bug du curseur sur mobile + Ajout Flavicon

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 const circle = document.querySelector('.circle');
 const trailContainer = document.querySelector('body');
 
+const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+
+if (!isTouchDevice) {
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+
+        circle.style.transform = `translate(${x}px, ${y}px)`;
+        createTrail(x, y);
+    });
+} else {
+    circle.remove();
+}
 
 function createTrail(x, y) {
+    if (isTouchDevice) return;
+
     const trail = document.createElement('div');
     trail.className = 'trail';
     trail.style.left = `${x}px`;
     trail.style.top = `${y}px`;
 
     trailContainer.appendChild(trail);
-
-
     setTimeout(() => trail.remove(), 100);
 }
-
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-
-    circle.style.transform = `translate(${x}px, ${y}px)`;
-
-   
-    createTrail(x, y);
-});
